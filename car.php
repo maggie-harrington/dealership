fc917c4802884c9401dfe86cc00ba0de638859b8
Leah Sherrell and Maggie Harrington <leah@>
2017-02-06 14:25:35 -0800

    encapsulate Car class by making all of its properties private and creating getters and setters for each property

--- car.php
+++ car.php
@@ -1,28 +1,76 @@
 <?php
 class Car
 {
-    public $make_model;
+    private $make_model;
     public $price;
     public $miles;
+    public $doors;
 
-    function __construct($make_model, $price, $miles)
+    function __construct($car_make_model, $car_price, $car_miles, $car_doors)
     {
-        $this->make_model = $make_model;
-        $this->price = $price;
-        $this->miles = $miles;
+        $this->make_model = $car_make_model;
+        $this->price = $car_price;
+        $this->miles = $car_miles;
+        $this->doors = $car_doors;
+    }
+
+    function setMakeModel($new_make_model)
+    {
+        $this->make_model = $new_make_model;
+    }
+
+    function setPrice ($new_price)
+    {
+        $float_price = (float) $new_price;
+        if ($float_price != 0) {
+            $formatted_price = number_format($float_price, 2);
+            $this->price = $formatted_price;
+        }
+    }
+
+    function setMiles($new_miles)
+    {
+        $this->miles = $new_miles;
+    }
+
+    function setDoors($new_doors)
+    {
+        $this->doors = $new_doors;
+    }
+
+    function getMakeModel()
+    {
+        return $this->make_model;
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
+    function getMiles()
+    {
+        return $this->miles;
+    }
+
+    function getDoors()
+    {
+        return $this->doors;
     }
 
 }
 
-$porsche = new Car("2014 Porsche 911", 114991, 7864);
-$ford = new Car("2011 Ford F450", 55995, 14241);
-$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+$car1 = new Car("2014 Porsche 911", 114991, 7864, 4);
+$car2 = new Car("2011 Ford F450", 55995, 14241, 4);
+$car3 = new Car("2013 Lexus RX 350", 44700, 20000, 4);
+$car4 = new Car("Mercedes Benz CLS550", 39900, 37979, 2);
 
-$cars = array($porsche, $ford, $lexus, $mercedes);
+$cars = array($car1, $car2, $car3, $car4);
 
 $cars_matching_search = array();
+
 foreach ($cars as $car) {
+    $car_price = $car->getPrice();
     if ($car->price < $_GET["price"]) {
         array_push($cars_matching_search, $car);
     }
@@ -40,10 +88,15 @@ foreach ($cars as $car) {
         <ul>
             <?php
                 foreach ($cars_matching_search as $car) {
-                    echo "<li> $car->make_model </li>";
+                    $current_make_model = $car->getMakeModel();
+                    $current_price = $car->getPrice();
+                    $current_miles = $car->getMiles();
+                    $current_doors = $car->getDoors();
+                    echo "<li> $current_make_model </li>";
                     echo "<ul>";
-                        echo "<li> $$car->price </li>";
-                        echo "<li> Miles: $car->miles </li>";
+                        echo "<li> $$current_price </li>";
+                        echo "<li> Miles: $current_miles </li>";
+                        echo "<li> Doors: $current_doors </li>";
                     echo "</ul>";
                 }
             ?>
