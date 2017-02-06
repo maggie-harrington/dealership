943dea2b6edfb41e4071d83db29ddbf7537130dd
Leah Sherrell and Maggie Harrington <maggie@>
2017-02-06 15:00:01 -0800

    add image property and paths for each object instance

--- car.php
+++ car.php
@@ -4,13 +4,15 @@ class Car
     private $make_model;
     private $price;
     private $miles;
+    private $image_path;
     private $doors;
 
-    function __construct($car_make_model, $car_price, $car_miles, $car_doors = 4)
+    function __construct($car_make_model, $car_price, $car_miles, $car_image, $car_doors = 4)
     {
         $this->make_model = $car_make_model;
         $this->price = $car_price;
         $this->miles = $car_miles;
+        $this->image_path = $car_image;
         $this->doors = $car_doors;
     }
 
@@ -38,6 +40,11 @@ class Car
         $this->doors = $new_doors;
     }
 
+    function setImage($new_image)
+    {
+        $this->image_path = $new_image;
+    }
+
     function getMakeModel()
     {
         return $this->make_model;
@@ -58,16 +65,21 @@ class Car
         return $this->doors;
     }
 
+    function getImage()
+    {
+        return $this->image_path;
+    }
+
     function worthBuying ($max_price)
     {
         return $this->price < $max_price;
     }
 }
 
-$car1 = new Car("2014 Porsche 911", 114991, 7864);
-$car2 = new Car("2011 Ford F450", 55995, 14241);
-$car3 = new Car("2013 Lexus RX 350", 44700, 20000);
-$car4 = new Car("Mercedes Benz CLS550", 39900, 37979, 2);
+$car1 = new Car("2014 Porsche 911", 114991, 7864, "image/Porsche.jpg");
+$car2 = new Car("2011 Ford F450", 55995, 14241, "image/Ford.jpg");
+$car3 = new Car("2013 Lexus RX 350", 44700, 20000, "image/Lexus.jpg");
+$car4 = new Car("Mercedes Benz CLS550", 39900, 37979, "image/Porsche.jpg", 2);
 
 $cars = array($car1, $car2, $car3, $car4);
 
@@ -95,11 +107,13 @@ foreach ($cars as $car) {
                     $current_price = $car->getPrice();
                     $current_miles = $car->getMiles();
                     $current_doors = $car->getDoors();
+                    $current_image = $car->getImage();
                     echo "<li> $current_make_model </li>";
                     echo "<ul>";
                         echo "<li> $$current_price </li>";
                         echo "<li> Miles: $current_miles </li>";
                         echo "<li> Doors: $current_doors </li>";
+                        echo "<li><img src='$current_image'></li>";
                     echo "</ul>";
                 }
             ?>
